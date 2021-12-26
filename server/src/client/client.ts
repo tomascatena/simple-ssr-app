8821112f55e00e46babeac970d07420c0248f2a2
Tomás Catena <tomascatena@gmail.com>
2021-12-26 22:28:28 +0100

    hydrate the app

--- server/src/client/client.ts
+++ server/src/client/client.ts
@@ -1,3 +1,7 @@
 // Startup point fo the client side application
 
-console.log('Hi there!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './components/Home'
+
+ReactDOM.hydrate(React.createElement(Home), document.querySelector('#root'))
