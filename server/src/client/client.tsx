d42254e72168adf800920b98b9ccc8aa7e9c7451
Tomás Catena <tomascatena@gmail.com>
2021-12-27 13:33:25 +0100

    setup client side redux store

--- server/src/client/client.tsx
+++ server/src/client/client.tsx
@@ -4,10 +4,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './AppRoutes';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export const store = configureStore({
+  reducer: {},
+  devTools: process.env.NODE_ENV !== 'production',
+});
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>,
-  document.querySelector('#root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </Provider>,
+
+  document.querySelector('#root'),
 );
