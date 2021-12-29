8be7c7727dc613e3762ec9f5396d97dd1900f5c4
Tomás Catena <tomascatena@gmail.com>
2021-12-29 21:38:56 +0100

    setup proxy

--- rendering-server/src/server.ts
+++ rendering-server/src/server.ts
@@ -3,8 +3,11 @@ import createStore from './helpers/createStore';
 import renderer from './helpers/renderer';
 import { matchRoutes, RouteObject } from 'react-router';
 import { RoutesArray } from './client/AppRoutes';
+import proxy from 'express-http-proxy';
 
-export type LoadData = (store: ReturnType<typeof createStore>) => Promise<unknown>;
+export type LoadData = (
+  store: ReturnType<typeof createStore>
+) => Promise<unknown>;
 
 interface RouteObjectWithLoadData extends RouteObject {
   loadData?: LoadData;
@@ -12,12 +15,25 @@ interface RouteObjectWithLoadData extends RouteObject {
 
 const app = express();
 
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      if (opts.headers) {
+        opts.headers['x-forward-host'] = 'localhost:3000';
+      }
+
+      return opts;
+    },
+  })
+);
+
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   const store = createStore();
 
-  const promises = matchRoutes(RoutesArray, req.path)?.map(match => {
+  const promises = matchRoutes(RoutesArray, req.path)?.map((match) => {
     const route: RouteObjectWithLoadData = match.route;
 
     return route.loadData ? route.loadData(store) : null;
